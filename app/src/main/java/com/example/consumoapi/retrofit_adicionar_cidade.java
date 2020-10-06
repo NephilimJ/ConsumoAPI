62608dedbf4ad8aafa6a41e55a4a98108ad79885
NephilimJ <renato_facul@outlook.com>
2020-10-05 22:48:21 -0300

    Inicio

---
+++ app/src/main/java/com/example/consumoapi/retrofit_adicionar_cidade.java
@@ -0,0 +1,14 @@
+package com.example.consumoapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class retrofit_adicionar_cidade extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retrofit_adicionar_cidade);
+    }
+}
